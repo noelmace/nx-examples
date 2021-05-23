db7cc824489e8ee052c7246941d1c1fc7c74f417
Noel Mace <contact@noelmace.com>
2021-05-23 23:56:53 +0200

    build: update migration script

--- migrate.sh
+++ migrate.sh
@@ -1,15 +1,19 @@
 set -e
 
 # VERSIONS=(8.9.0 9.0.4 9.1.4 9.2.4 9.3.0 9.4.5 9.5.1 9.6.0 9.7.1 9.8.0 10.0.13 10.1.0 10.2.1 10.3.3 10.4.15 11.0.20 11.1.5 11.2.12 11.3.2 11.4.0 11.5.2 11.6.3 12.0.8 12.1.1 12.2.0 12.3.4)
-VERSIONS=(11.0.20 11.1.5 11.2.12 11.3.2 11.4.0 11.5.2 11.6.3 12.0.8 12.1.1 12.2.0 12.3.4)
+VERSIONS=(12.0.8 12.1.1 12.2.0 12.3.4)
 
 CURRENT_NX_VERSION=$(awk -F \" '/"@nrwl\/workspace": ".+"/ { print $4; exit; }' package.json)
 
 echo "Current Nx Version: $CURRENT_NX_VERSION"
 
 show-git() {
+  echo
+  echo "--- git ---"
   git log --oneline --decorate -n 1
   git status -s
+  echo "----------"
+  echo
 }
 
 notify-and-stop() {
